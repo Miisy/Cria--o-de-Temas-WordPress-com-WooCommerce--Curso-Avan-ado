bf2e6932a1f2d9b531d828f2b80494cff7e69d92
Thamires Sales <thamisales@hotmail.com>
2024-11-01 17:55:12 -0300

    First few changes

---
+++ wp-content/themes/cursinho/footer.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Fancy Lab
+ */
+
+?>
+<footer>
+			<section class="footer-widgets">
+
+			</section>
+			<section class="copyright">
+				
+			</section>
+		</footer>
+	</div>
+</body>
+</html>
